38cfee7cb200c3e24589f2a47e00c6facee7d6d3
petar petrovic <njegos1803981@gmail.com>
2017-06-20 10:58:54 +0200

    dodatxml

---
+++ app/src/main/java/example11/glumci20/Druga.java
@@ -0,0 +1,14 @@
+package example11.glumci20;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Druga extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_druga);
+    }
+}
