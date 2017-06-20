791ac2111bd9c7ec0ebc58c209d62285f890ab4a
petar petrovic <njegos1803981@gmail.com>
2017-06-20 11:19:47 +0200

    dodatint

--- app/src/main/java/example11/glumci20/Prva.java
+++ app/src/main/java/example11/glumci20/Prva.java
@@ -4,7 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Prva extends Activity {
-
+private int a=1;
+    private int b=8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
